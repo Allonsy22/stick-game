072ae806986bdae7961a2b81bb4674c696eeeadc
Alexander Grits <a.g.allonsy@gmail.com>
2020-11-18 20:27:42 +0200

    server side game logic

---
+++ server/game/Game.js
@@ -0,0 +1,115 @@
+class Game {
+  constructor() {
+    this.size = null;
+    this.availableMoves = [];
+    this.squares = [];
+  };
+
+  newGame(props) {
+    const { size = 3 } = props;
+    this.size = 2 * size - 1;
+    this.availableMoves = [];
+    this.squares = [];
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        if (i % 2 === 0 && j % 2 !== 0) {
+          this.availableMoves.push({ i, j });
+          continue;
+        }
+        if (i % 2 !== 0 && j % 2 === 0) {
+          this.availableMoves.push({ i, j });
+          continue;
+        }
+        if (i % 2 !== 0 && j % 2 !== 0) {
+          this.squares.push(this.getSquare(i, j));
+        }
+      }
+    }
+  }
+
+  makeMove(i, j) {
+    const filledSquareCoords = [];
+    if (this.isAvalaibleMove(i, j)) {
+      this.availableMoves = this.availableMoves.filter(coords => {
+        return coords.i !== i || coords.j !== j;
+      });
+      this.squares.forEach( square => {
+        if (square.isSquareFilled(i, j)) {
+          filledSquareCoords.push(square.getSquareCoords());
+        }
+      });
+
+    }
+    return false;
+  }
+
+  isAvalaibleMove(i, j) {
+    let isAvailable = false;
+    this.availableMoves.forEach(coords => {
+      if (coords.i === i && coords.j === j) {
+        isAvailable = true;
+        return;
+      }
+    })
+    return isAvailable;
+  }
+
+  isNextTurn() {
+    
+  }
+
+  getSquare(i, j) {
+    const top = {i: i - 1, j};
+    const right = {i, j: j + 1};
+    const bottom = {i: i + 1, j};
+    const left = {i, j: j - 1};
+    return new Square({squareCoords: {i, j}, top, right, bottom, left});
+  }
+  
+  getAvailableMoves() {
+    return this.availableMoves;
+  }
+}
+
+class Square {
+  constructor(props) {
+    const { squareCoords, top, right, bottom, left } = props;
+    // squareCoords, top, right, bottom, left - are all object with coords
+    this.top = top;
+    this.right = right;
+    this.bottom = bottom;
+    this.left = left;
+    this.squareCoords = squareCoords;
+    this.owned = false;
+  }
+
+  getSquareCoords() {
+    return this.squareCoords;
+  }
+
+  isSquareFilled(i, j) {
+    this.fillLine(i, j);
+    if (!this.owned 
+      && this.top === 'filled'
+      && this.right === 'filled'
+      && this.bottom === 'fillde'
+      && this.left === 'filled') {
+        this.owned = true;
+        return true;
+      };
+    return false;
+  }
+
+  fillLine(i, j) {
+    if (this.checkLine(this.top, i, j)) this.top = 'filled';
+    if (this.checkLine(this.right, i, j)) this.right = 'filled';
+    if (this.checkLine(this.bottom, i, j)) this.bottom = 'filled';
+    if (this.checkLine(this.left, i, j)) this.left = 'filled';
+  }
+
+  checkLine(line, i, j) {
+    return line.i === i && line.j === j;
+  }
+}
+
+module.exports = Game;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const Game = require('./game/Game');
+const game = new Game();
+game.newGame({ size: 3 });
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors({ origin: true }));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3333;
 
 app.get('/', (req, res) => {
-  res.status(200).json('It works');
+  console.log(game.getAvailableMoves());
+  res.status(200).json(game.getAvailableMoves());
 });
 
-app.listen(port, () => {
+app.post('/', (req, res) => {
+  const i = req.body.i;
+  const j = req.body.j;
+  res.status(200).json(game.makeMove(i, j));
+})
+
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+const io = require('socket.io')(server, { origins: 'http://localhost:3000/' });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('test', (data) => {
+    console.log(data);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
