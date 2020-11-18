8557a05e34e46ba493eaeab6e8a1cf4e26da9b19
Alexander Grits <a.g.allonsy@gmail.com>
2020-11-18 19:45:31 +0200

    add socketApi and updateObject utils

---
+++ client/src/utils/socketApi.js
@@ -0,0 +1,47 @@
+import socketIOClient from "socket.io-client";
+const URI = "http://localhost:3333";
+
+export default class socketAPI {
+  socket;
+
+  connect() {
+    this.socket = socketIOClient(URI, {transports: ['websocket', 'polling', 'flashsocket']});
+    return new Promise((resolve, reject) => {
+      this.socket.on('connection', () => resolve());
+      this.socket.on('connect_error', (error) => reject(error));
+    });
+  }
+
+  disconnect() {
+    return new Promise((resolve) => {
+      this.socket.disconnect(() => {
+        this.socket = null;
+        resolve();
+      });
+    });
+  }
+
+  emit(event, data) {
+    return new Promise((resolve, reject) => {
+      if (!this.socket) return reject('No socket connection.');
+
+      return this.socket.emit(event, data, (response) => {
+        if (response.error) {
+          console.error(response.error);
+          return reject(response.error);
+        }
+
+        return resolve();
+      });
+    });
+  }
+
+  on(event, fun) {
+    return new Promise((resolve, reject) => {
+      if (!this.socket) return reject('No socket connection.');
+
+      this.socket.on(event, fun);
+      resolve();
+    });
+  }
+}
