b66a4eae625cc103694121bcec10944ba838d3f2
Alexander Grits <a.g.allonsy@gmail.com>
2020-11-19 15:11:44 +0200

    added Line component

---
+++ client/src/componets/Canvas/Line/Line.js
@@ -0,0 +1,43 @@
+import React, {Component } from 'react';
+import { connect} from 'react-redux';
+import { isCoordsInArray } from '../../../utils/isCoordsInArray';
+import * as actions from '../../../store/actions/index';
+
+class Line extends Component {
+  makeMove(props) {
+    const { i, j } = props;
+    const isCoords = isCoordsInArray({
+      coords: {i, j},
+      array: this.props.availableMoves
+    });
+    if (isCoords && this.props.isNextTurn) this.props.makeMove({i, j});
+  }
+  render() {
+    const { i, j, type, owner } = this.props;
+    const className = `${type} ${owner}`;
+    return (
+      <div
+        className={className}
+        i={i} j={j}
+        key={i + j}
+        onClick={() => this.makeMove({ i, j })}
+      ></div>
+    );
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    isNextTurn: state.game.isNextTurn,
+    availableMoves: state.game.availableMoves,
+  };
+};
+
+
+const mapDispatchToProps = dispatch => {
+  return {
+    makeMove: (coords) => dispatch(actions.makeMove(coords)),
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Line);
