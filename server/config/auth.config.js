1432d4be2ae16f500a16b36aa0a40d05ac2de49d
Alexander Grits <a.g.allonsy@gmail.com>
2020-11-20 22:34:57 +0200

    deleted config folder

--- server/config/auth.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  secret: "allonsysuper-secure-key",
-};
--- server/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "allonsy53306",
-  DB: "users",
-  dialect: "postgres",
-  PORT: "5432",
-};
