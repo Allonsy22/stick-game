3f47fe263012ccb691e24d994f1d73364c8667f1
Alexander Grits <a.g.allonsy@gmail.com>
2020-11-24 19:49:47 +0200

    modified toolbar

--- client/src/componets/Toolbar/Toolbar.js
+++ client/src/componets/Toolbar/Toolbar.js
@@ -5,20 +5,50 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Alert from 'react-bootstrap/Alert';
+import Modal from 'react-bootstrap/Modal';
 
 class Toolbar extends Component {
+  state = {
+    showDialog: false,
+  };
+
+  onStatisticsButtonClick() {
+    const { getPlayerStatistics } = this.props;
+    getPlayerStatistics();
+    this.setState({showDialog: true});
+  };
+
+  renderStatisticsDialog() {
+    const { showDialog } = this.state;
+    const { statistics: {totalGames, winning, winRate} } = this.props;
+    return (
+      <Modal show={showDialog} onHide={() => { }}>
+        <Modal.Header>
+          <Modal.Title>My statistics</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Total games: {totalGames}</p>
+          <p>Winning: {winning}</p>
+          <p>Win rate: {winRate}%</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="success" onClick={() => {this.setState({showDialog: false})}}>OK</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
   renderButtonGroup() {
-    const { logout, getPlayerStatistics } = this.props;
+    const { logout } = this.props;
     return (
       <ButtonGroup>
-        <Button 
-          className="mr-2" 
-          onClick={() => getPlayerStatistics()}
+        <Button
+          className="mr-2"
+          onClick={() => this.onStatisticsButtonClick()}
         >
           Statistics
         </Button>
-        <Button onClick={() => logout()}>Log Out</Button>
+        <Button className="mr-2" onClick={() => logout()}>Log Out</Button>
       </ButtonGroup>
     );
   };
@@ -27,6 +57,7 @@ class Toolbar extends Component {
     const { user } = this.props;
     return (
       <Container className="mt-4">
+        {this.renderStatisticsDialog()}
         <Row className="justify-content-end">
           {user && this.renderButtonGroup()}
         </Row>
@@ -38,6 +69,7 @@ class Toolbar extends Component {
 const mapStateToProps = state => {
   return {
     user: state.auth.user,
+    statistics: state.stats.statistics,
   };
 }
 
